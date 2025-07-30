05f1a5c5dc703eae378c378d01eda556decfda09
aAAiman <aimanreviva@gmail.com>
2025-07-30 15:15:23 +0000

    mengbah ke nama domain

--- server.js
+++ server.js
@@ -54,6 +54,3 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
