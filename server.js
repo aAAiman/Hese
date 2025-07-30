5b82dab975f401c94150543dd20762be9d869f45
aAAiman <aimanreviva@gmail.com>
2025-07-30 22:57:48 +0700

    move server start logic to the end of the file

--- server.js
+++ server.js
@@ -54,3 +54,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
+
